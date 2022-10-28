76d460eb2561208c32ba677d1d85945598fc59e8
VPagent <vpagent13@gmail.com>
2022-10-28 15:17:15 +0300

    fix22

--- src/pages/SingleProductPage/SingleProductPage.jsx
+++ src/pages/SingleProductPage/SingleProductPage.jsx
@@ -1,3 +1,9 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import ItemCard from '../../components/ItemCard/ItemCard';
+import store from '../../globalState/store';
+import servicesApi from '../../services/API';
+import singleCardStyles from './singleCardStyle';
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,12 +13,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import ItemCard from '../../components/ItemCard/ItemCard';
-import store from '../../globalState/store';
-import servicesApi from '../../services/API';
-import singleCardStyles from './singleCardStyle';
+
 const SingleProductPage = () => {
     const [isSinglePage, setIsSinglePage] = store.useGlobalState("isSinglePage");
     const [singleItem, setSingleItem] = store.useGlobalState("singleItem");
